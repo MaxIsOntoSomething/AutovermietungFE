bf1a226bd2707d111ba90db2a9fcf0e4a5c3864a
MaxIsOntoSomething <149909243+MaxIsOntoSomething@users.noreply.github.com>
2024-04-07 22:08:18 +0200

    Tokens speichern, admin dashboard , routing

--- src/app/auth/components/login/login.component.ts
+++ src/app/auth/components/login/login.component.ts
@@ -2,47 +2,63 @@ import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { AuthService } from '../../services/auth/auth.service';
 import { StorageService } from '../../services/storage/storage.service';
+import { Router } from '@angular/router';
+import { NzMessageService } from 'ng-zorro-antd/message';
+// import { UserStorageService } from '../services/storage/user-storage.service';
+// 
+
+
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrl: './login.component.scss'
+  styleUrls: ['./login.component.scss']
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
 
-  isSpinning:boolean = false;
-  loginForm! : FormGroup;
+  validateForm!: FormGroup;
+  isSpinning = false;
 
-  constructor (private fb:FormBuilder,
-  private authService: AuthService) {}
-
-  ngOnInit() {
-    this.loginForm = this.fb.group({
-      email: [null,[Validators.email,Validators.required]],
-      password: [null, [Validators.required]]
-  
-    });
-  }
-  login(){
-    console.log(this.loginForm.value);
-    this.authService.login(this.loginForm.value).subscribe((res)=>{
-      console.log(res);
-      if(res.userId != null){
-        const user = {
-          id: res.userId,
-          role:res.userRole
+  submitForm(): void {
+    this.isSpinning = true;
+    this.authService.login(this.validateForm.value).subscribe(
+      (res) => {
+        if (res.userId != null) {
+          console.log(res);
+          const user = {
+            id: res.userId,
+            role: res.userRole
+          }
+          console.log(user);
+          StorageService.saveUser(user);
+          StorageService.saveToken(res.jwt);
+          if (StorageService.isAdminLoggedIn()) {
+            this.router.navigateByUrl('admin/dashboard');
+          } else if (StorageService.isCustomerLoggedIn()) {
+            this.router.navigateByUrl('customer/dashboard');
+          }
+        } else {
+          this.message
+            .error(
+              `Bad credentials`,
+              { nzDuration: 5000 }
+            )
         }
-        StorageService.saveUser(user);
-        StorageService.saveToken(res.jwt);
+        this.isSpinning = false;
+      })
+  }
 
+  constructor(private fb: FormBuilder,
+    private authService: AuthService,
+    private message: NzMessageService,
+    private router: Router,) { }
 
-
-      }
-
-
-
-
+  ngOnInit(): void {
+    this.validateForm = this.fb.group({
+      username: [null, [Validators.required]],
+      password: [null, [Validators.required]],
     });
   }
 
 
+  
 }
--- src/app/auth/services/storage/storage.service.ts
+++ src/app/auth/services/storage/storage.service.ts
@@ -20,9 +20,33 @@ static saveToken(token: string): void {
 static saveUser(user: any): void {
   window.localStorage.removeItem(USER);
   window.localStorage.setItem(USER, JSON.stringify(user));
- 
- 
- }
+} // Add this closing brace
+
+static getToken() {
+  return window.localStorage.getItem(TOKEN);
+}
+
+static getUser() {
+  return  JSON.parse(localStorage.getItem(USER) || '{}');
+
+}
+
+static getUserRole():string {
+  const user = this.getUser();
+  if(user == null) return "";
+  return user.role;
+}
+
+static isAdminLoggedIn():boolean {
+  if(this.getToken() == null) return false;
+  const role :string = this.getUserRole();
+  return  role == "ADMIN";
+}
+static isCustomerLoggedIn():boolean {
+  if(this.getToken() == null) return false;
+  const role :string = this.getUserRole();
+  return  role == "CUSTOMER";
+}
 
 
 }
