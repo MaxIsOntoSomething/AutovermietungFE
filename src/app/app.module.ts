b5a4dff07cfd9e64f9c2b594fe50162dc342f5dc
MaxIsOntoSomething <149909243+MaxIsOntoSomething@users.noreply.github.com>
2024-04-07 22:34:33 +0200

    imports neu

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,7 +10,7 @@ import en from '@angular/common/locales/en';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { DemoNgZorroAntdModule } from './DemoNgZorroAntdModule';
+import { DemoNgZorroAntdModule } from "./DemoNgZorroAntdModule;
 import { LoginComponent } from './auth/login/login.component';
 import { SignupComponent } from './auth/signup/signup.component';
 
