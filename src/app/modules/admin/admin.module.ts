1092c854fdf52350b4569901038141476ed535b5
MaxIsOntoSomething <149909243+MaxIsOntoSomething@users.noreply.github.com>
2024-04-07 22:18:52 +0200

    Admin routing

--- src/app/modules/admin/admin.module.ts
+++ src/app/modules/admin/admin.module.ts
@@ -1,17 +1,28 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 import { AdminRoutingModule } from './admin-routing.module';
-import { AdminDashboardComponent } from './components/admin-dashboard/admin-dashboard.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { AddCarComponent } from './components/add-car/add-car.component';
+import { UpdateCarComponent } from './components/update-car/update-car.component';
+import { SearchCarComponent } from './components/search-car/search-car.component';
+import { GetBookingsComponent } from './components/get-bookings/get-bookings.component';
+import { DemoNgZorroAntdModule } from 'src/app/DemoNgZorroAntdModule';
 
 
 @NgModule({
   declarations: [
-    AdminDashboardComponent
+    DashboardComponent,
+    AddCarComponent,
+    UpdateCarComponent,
+    SearchCarComponent,
+    GetBookingsComponent
   ],
   imports: [
     CommonModule,
-    AdminRoutingModule
+    AdminRoutingModule,
+    DemoNgZorroAntdModule,
+    ReactiveFormsModule
   ]
 })
 export class AdminModule { }
